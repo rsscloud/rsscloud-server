b7b6490932a85f33b38016b3f5aca6832f59692c
Andrew Shell <andrew@andrewshell.org>
2015-03-05 11:17:20 -0600

    Allow viewLog to return json if accepts header asks for it

--- controllers/view-log.js
+++ controllers/view-log.js
@@ -1,15 +1,40 @@
 "use strict";
 
+var async = require('async');
 var express = require('express');
 var router = express.Router();
+var rssCloudSuite = require('../services/suite');
 var safefs = require('../services/safefs');
 
-router.get('/', function (req, res) {
-    safefs.watchStruct('data', function (errorMessage, data) {
-        if (errorMessage) {
-            return res.send(errorMessage);
-        }
+function processResponse(req, res, data) {
+    switch (req.accepts('html', 'json')) {
+    case 'html':
         res.render('view-log', data);
+        break;
+    case 'json':
+        res.json(data.eventlog);
+        break;
+    default:
+        res.status(406).send('Not Acceptable');
+        break;
+    }
+}
+
+function handleError(req, res, errorMessage) {
+    processResponse(req, res, rssCloudSuite.errorResult(errorMessage));
+}
+
+router.get('/', function (req, res) {
+    async.waterfall([
+        function (callback) {
+            safefs.watchStruct('data', callback);
+        },
+        function (data) {
+            console.log(data);
+            processResponse(req, res, data);
+        }
+    ], function (errorMessage) {
+        handleError(req, res, errorMessage);
     });
 });
 
